bfa0a5b7503d0720c42d619a1c59d352ef087518
Paulina Kujawa <paulina.kujawa1991@gmail.com>
2022-08-10 18:20:48 +0200

    Refactor tests to reflect Arrange Act Assert approach

--- tests/login_page_test.py
+++ tests/login_page_test.py
@@ -9,19 +9,26 @@ class TestOpenLoginPage:
 
     def test_log_in_correct_data(self):
         login_page = LoginPage(self.driver)
+
         login_page.log_in(const.login,const.password)
+
         assert login_page.is_login_successfuly()
 
     def test_log_in_invalid_login(self):
         invalid_login = str(random.randint(0,1000)) + const.login
+        expected_msg = 'The username ' + invalid_login + ' is not registered on this site. If you are unsure of your username, try your email address instead.'
         login_page = LoginPage(self.driver)
+
         login_page.log_in(invalid_login, const.password)
-        msg= 'The username ' + invalid_login + ' is not registered on this site. If you are unsure of your username, try your email address instead.'
-        assert msg in login_page.get_error_message()
+
+        assert expected_msg in login_page.get_error_message()
 
     def test_log_in_invalid_password(self):
         invalid_password = str(random.randint(0,1000)) + const.password
+        expected_msg = 'The password you entered for the username'
         login_page = LoginPage(self.driver)
+
         login_page.log_in(const.login, invalid_password)
-        msg = 'The password you entered for the username'
-        assert msg in login_page.get_error_message()
+
+
+        assert expected_msg in login_page.get_error_message()
--- tests/new_post_test.py
+++ tests/new_post_test.py
@@ -1,48 +1,55 @@
 import random
 import pytest
 from helpers import helpers
-from pages.new_post_page import AddNewPost
+from pages.new_post_page import NewPostPage
 from pages.posts_page import PostPage
 from tests import const
 
 
 @pytest.mark.usefixtures('setup')
 class TestNewPost:
+    title = 'New title_unique no:' + str(random.randint(0, 1000))
 
     def test_correctly_saving_for_title(self):
-        add_new_post = AddNewPost(self.driver)
-        add_new_post.log_in(const.login, const.password)
-        title = 'New title_unique no:' + str(random.randint(0, 1000))
-        add_new_post.new_post_title(title)
-        add_new_post.publish_post()
+        new_post = NewPostPage(self.driver)
+
+        new_post.log_in(const.login, const.password)
+        new_post.create_new_post()
+        new_post.set_post_title(self.title)
+        new_post.publish_post()
         post_page = PostPage(self.driver)
-        assert title == post_page.correct_displayed_data_for_list()
+
+        assert self.title == post_page.correct_displayed_data_for_list()
 
     def test_correctly_saving_for_list(self):
-        add_new_post = AddNewPost(self.driver)
-        add_new_post.log_in(const.login, const.password)
-        title = 'New title for list'
         text1 = 'new text for 1th row unique text no:' + str(random.randint(0, 1000))
         text2 = 'new text for 2nd row unique text no:' + str(random.randint(0, 1000))
         text3 = 'the last text for 3rd row unique text no:' + str(random.randint(0, 1000))
-        add_new_post.new_post_title(title)
-        add_new_post.add_block_list()
-        add_new_post.add_item_to_list(text1)
-        add_new_post.add_item_to_list(text2)
-        add_new_post.add_item_to_list(text3)
-        add_new_post.publish_post()
+        new_post = NewPostPage(self.driver)
+
+        new_post.log_in(const.login, const.password)
+        new_post.create_new_post()
+        new_post.set_post_title(self.title)
+        new_post.add_block_list()
+        new_post.add_item_to_list(text1)
+        new_post.add_item_to_list(text2)
+        new_post.add_item_to_list(text3)
+        new_post.publish_post()
         post_page = PostPage(self.driver)
+
         assert text1 + '\n' + text2 + '\n' + text3 == post_page.get_data_for_list()
 
     def test_correctly_saving_for_quote(self):
-        add_new_post = AddNewPost(self.driver)
-        add_new_post.log_in(const.login, const.password)
-        title = 'New title_unique no:' + str(random.randint(0, 1000))
         quote = 'To be or not to be unique text:' + helpers.get_random_string()
         author = 'WS' + helpers.get_random_string()
-        add_new_post.new_post_title(title)
-        add_new_post.add_block_quote(quote, author)
-        add_new_post.publish_post()
+        new_post = NewPostPage(self.driver)
+
+        new_post.log_in(const.login, const.password)
+        new_post.create_new_post()
+        new_post.set_post_title(self.title)
+        new_post.add_block_quote(quote, author)
+        new_post.publish_post()
         post_page = PostPage(self.driver)
+
         assert quote == post_page.get_quote()
         assert author == post_page.get_quote_author()
